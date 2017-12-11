784b6151e252bd84dab126e07c575a7ff0c33fd0
Giorvi S Alvarez <ga73724@srcs.loc>
2017-12-11 12:20:55 -0800

    hj

--- functions.js
+++ functions.js
@@ -1,10 +1,19 @@
 function checkPrime(number) {
 
-      if (number > 1 && number % 1 === 0 || ==2) {
+      if (number % 1 === 0 || ==2) {
         return "Prime";
 
+    } else if (number > 1) {
+        for (var i = 2; i < number/2; i++){
+            if(number % i == 0){
+
+                return "Prime"
+            }
+        }
+
     } else {
+
      return "Not Prime";
-    }
+
 
 }
